e930f9639b382b039ddf48512576fa74953f8f6e
Nick <soehner.nick@gmail.com>
2022-05-10 21:35:51 -0600

    added media adjustments and animations

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ const explore = document.getElementById("explore-box");
 const details = document.getElementById("details-box");
 const plusIcon = document.getElementById("plus-icon");
 const closeBtn = document.getElementById("close-btn");
+const textBox = document.getElementById("text-box");
+const textContainer = document.getElementById("text-container");
 
 plusIcon.addEventListener("click", function () {
-	details.classList.toggle("d-none");
-	explore.classList.toggle("d-none");
+	plusIcon.classList.add("pressed");
+	textBox.classList.remove("slide-out");
+	details.classList.remove("fade-out");
+	textContainer.classList.toggle('text-container')
+	textContainer.classList.toggle('text-container-hidden')
+	setTimeout(() => {
+		details.classList.toggle("d-none");
+		explore.classList.toggle("d-none");
+		plusIcon.classList.remove("pressed");
+	}, 300);
 });
 
 closeBtn.addEventListener("click", function () {
-	details.classList.toggle("d-none");
-	explore.classList.toggle("d-none");
+	closeBtn.classList.add("pressed");
+	textBox.classList.add("slide-out");
+	explore.classList.remove("fade-out");
+	textContainer.classList.toggle('text-container-hidden')
+	textContainer.classList.toggle('text-container')
+	setTimeout(() => {
+		details.classList.toggle("d-none");
+		explore.classList.toggle("d-none");
+		closeBtn.classList.remove("pressed");
+		plusIcon.classList.add("pressed");
+	}, 500);
+	setTimeout(() => {
+		plusIcon.classList.remove("pressed");
+	}, 1000)
 });
